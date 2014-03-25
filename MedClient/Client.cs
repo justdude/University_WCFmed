de25e76e08c5e9f2b4e3584176577f709d803810
Ivan Albantov <dude.just.dude@gmai.com>
2014-03-25 03:55:48 +0200

    med client commit

---
+++ MedClient/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedClient.ServiceReference1;
+
+namespace MedClient
+{
+    class Client
+    {
+        //JsonFxAdapter<Connector>.LoadJson()
+
+        private static Client _client = null;
+        public static Client ClientInstance
+        {
+            get
+            {
+                if (_client == null) _client = new Client();
+                return _client;
+            }
+        }
+
+
+        private MedWCFClient _medClient = null;
+        public MedWCFClient MedClient
+        {
+            get
+            {
+                if (_medClient == null) _medClient = new MedWCFClient();
+                return _medClient;
+            }
+        }
+
+
+    }
+}
---
+++ MedClient/JsonHandlerAdapter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.IO;
+using JsonFx.Json;
+using JsonFx.Xml;
+/// <summary>
+/// Json fx adapter. Adapter for Json FX library
+/// </summary>
+public class JsonFxAdapter<T>
+{
+	
+	public static void SaveXML(string path,T[] objects)
+	{
+		string str = JsonWriter.Serialize(objects);
+		//Debug.Log(str.ToString());
+		StreamWriter wr = new StreamWriter(path);
+		wr.WriteLine(str);
+		wr.Close();
+	}
+	public static T[] LoadXML(string path)
+	{
+		StreamReader reader = new StreamReader(path);
+		 string text = reader.ReadToEnd();
+		//Debug.Log(text);
+		reader.Close();
+		T[] arr1 = JsonReader.Deserialize<T[]>(text);
+		return arr1;
+	}
+	
+	public static void SaveJson(string path,T[] objects)
+	{
+		string str = JsonWriter.Serialize(objects);
+		//Debug.Log(str.ToString());
+		StreamWriter wr = new StreamWriter(path);
+		wr.WriteLine(str);
+		wr.Close();
+	}
+	public static T[] LoadJson(string path)
+	{
+		StreamReader reader = new StreamReader(path);
+		 string text = reader.ReadToEnd();
+		//Debug.Log(text);
+		reader.Close();
+		
+		T[] arr1 = JsonReader.Deserialize<T[]>(text);
+		return arr1;
+	}
+	public static T[] LoadJsonFromString(string json)
+	{
+		T[] arr1 = JsonReader.Deserialize<T[]>(json);
+		return arr1;
+	}
+}
